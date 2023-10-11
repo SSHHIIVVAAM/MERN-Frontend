6c77b75792a84531ed34bb5183b123d56e1b9a52
SSHHIIVVAAM <shivammanhas119@gmail.com>
2023-10-11 15:30:41 +0530

    fourth commit

--- src/features/product-list/productSlice.js
+++ src/features/product-list/productSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './productListAPI';
+import { fetchCount } from '../product-list/productAPI';
 
 const initialState = {
   value: 0,
