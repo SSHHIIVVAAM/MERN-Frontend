a99a6c33809f815d1c1572970f9a6a86b2248477
SSHHIIVVAAM <shivammanhas119@gmail.com>
2023-10-12 14:05:10 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
 import Cart from './features/cart/Cart';
 import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
-import ProductDetail from './features/product-list/components/ProductDetail';
+import ProductDetail from './features/product/components/ProductDetail';
 import ProductDetailPage from './pages/ProductDetailPage';
 
 const router = createBrowserRouter([
