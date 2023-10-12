be39aff36a54a29f67afa0533cbb31ec6e745d7e
SSHHIIVVAAM <shivammanhas119@gmail.com>
2023-10-12 16:36:31 +0530

    seventh  commit

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -18,14 +18,14 @@ export const fetchAllProductsAsync = createAsyncThunk(
 
 export const fetchProductsByFiltersAsync = createAsyncThunk(
   'product/fetchProductsByFilters',
-  async (filter) => {
-    const response = await fetchProductsByFilters(filter);
+  async ({filter,sort}) => {
+    const response = await fetchProductsByFilters(filter,sort);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
-export const productSlice = createSlice({
+export const productSlice = createSlice({ 
   name: 'product',
   initialState,
   reducers: {
