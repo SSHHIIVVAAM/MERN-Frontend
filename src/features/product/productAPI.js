a0ee1b3f60f12cea9e222318d255ec5a028c6ef2
SSHHIIVVAAM <shivammanhas119@gmail.com>
2023-10-13 10:27:36 +0530

    eight  commit

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,9 +8,10 @@ export function fetchAllProducts() {
   );
 }
 
-export function fetchProductsByFilters(filter,sort) {
+export function fetchProductsByFilters(filter,sort,pagination) {
   // filter = {"category":["smartphone","laptops"]}
   // sort = {_sort:"price",_order="desc"}
+
   // TODO : on server we will support multi values
   let queryString = '';
   for (let key in filter){
@@ -23,12 +24,17 @@ export function fetchProductsByFilters(filter,sort) {
   for(let key in sort){
     queryString += `${key}=${sort[key]}&`
   }
+  console.log(pagination)
+  for(let key in pagination){
+    queryString += `${key}=${pagination[key]}&`
+  }
 
   return new Promise(async (resolve) =>{
     // TODO: we will not hard-code server URL here
     const response = await fetch('http://localhost:8080/products?'+queryString)
     const data = await response.json()
-    resolve({data})
+    const totalItems = await response.headers.get('X-Total-Count')
+    resolve({data:{products:data,totalItems:+totalItems}})
 }
   );
 }
