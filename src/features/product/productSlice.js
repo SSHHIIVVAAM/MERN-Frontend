6f99d717c852e9938d910c08cfe91507a7dd69da
SSHHIIVVAAM <shivammanhas119@gmail.com>
2023-11-26 10:25:40 +0530

    Backend Part-4  commit

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -17,14 +17,6 @@ const initialState = {
   totalItems: 0,
   selectedProduct: null,
 };
-export const fetchAllProductsAsync = createAsyncThunk(
-  "product/fetchAllProducts",
-  async () => {
-    const response = await fetchAllProducts();
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
-  }
-);
 
 export const fetchProductByIdAsync = createAsyncThunk(
   "product/fetchProductById",
@@ -36,8 +28,8 @@ export const fetchProductByIdAsync = createAsyncThunk(
 );
 export const fetchProductsByFiltersAsync = createAsyncThunk(
   "product/fetchProductsByFilters",
-  async ({ filter, sort, pagination }) => {
-    const response = await fetchProductsByFilters(filter, sort, pagination);
+  async ({ filter, sort, pagination, admin }) => {
+    const response = await fetchProductsByFilters(filter, sort, pagination, admin);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
@@ -87,13 +79,6 @@ export const productSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchAllProductsAsync.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-        state.status = "idle";
-        state.products = action.payload;
-      })
       .addCase(fetchProductsByFiltersAsync.pending, (state) => {
         state.status = "loading";
       })
